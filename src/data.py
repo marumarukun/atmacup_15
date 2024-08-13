fee1e4a6de9bfe4e1f50cb91c31ada75b15ade24
Masato Ueda <cheriera.0310@gmail.com>
2024-08-13 15:38:42 +0900

    fix: ラベルエンコーディング部を外に出した

--- src/data.py
+++ src/data.py
@@ -10,22 +10,21 @@ def load_data():
     test = pl.read_csv(f"../{INPUT_DIR}/test.csv", try_parse_dates=True)
     anime = pl.read_csv(f"../{INPUT_DIR}/anime.csv", try_parse_dates=True)
 
-    # label encoding(user_id)
+    return train, test, anime
+
+
+def user_id_label_encoding(train, test):
     le_user_id = LabelEncoder()
     le_user_id.fit(pl.concat([train["user_id"], test["user_id"]]).fill_null(""))
-    train = train.with_columns(pl.Series("user_id", le_user_id.transform(train["user_id"])).alias("user_id"))
-    test = test.with_columns(pl.Series("user_id", le_user_id.transform(test["user_id"])).alias("user_id"))
+    train = train.with_columns(pl.Series(le_user_id.transform(train["user_id"])).alias("user_id"))
+    test = test.with_columns(pl.Series(le_user_id.transform(test["user_id"])).alias("user_id"))
+    return train, test
 
-    # label encoding(anime_id)
+
+def anime_id_label_encoding(train, test, anime):
     le_anime_id = LabelEncoder()
     le_anime_id.fit(anime["anime_id"])
-    anime = anime.with_columns(pl.Series("anime_id", le_anime_id.transform(anime["anime_id"])).alias("anime_id"))
-    train = train.with_columns(pl.Series("anime_id", le_anime_id.transform(train["anime_id"])).alias("anime_id"))
-    test = test.with_columns(pl.Series("anime_id", le_anime_id.transform(test["anime_id"])).alias("anime_id"))
-
-    # データ型を縮小
-    anime = anime.select(pl.all().shrink_dtype())
-    train = train.select(pl.all().shrink_dtype())
-    test = test.select(pl.all().shrink_dtype())
-
+    anime = anime.with_columns(pl.Series(le_anime_id.transform(anime["anime_id"])).alias("anime_id"))
+    train = train.with_columns(pl.Series(le_anime_id.transform(train["anime_id"])).alias("anime_id"))
+    test = test.with_columns(pl.Series(le_anime_id.transform(test["anime_id"])).alias("anime_id"))
     return train, test, anime
